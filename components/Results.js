a5d66a07c93876c963c51df83783f000fae52c35
Gee Markwell <geemarkwell@gmail.com>
2021-05-17 23:31:29 -0600

    ..

--- components/Results.js
+++ components/Results.js
@@ -6,7 +6,7 @@ function Results({ results }) {
         <FlipMove className='px-5 my-10 sm:grid py-6 md:grid-cols-2 xl:grid-cols-3 
         3xl:flex flex-wrap justify-center'>
             {results.map((result) => (
-                <Thumbnail key={results.id} result={result}  />
+                <Thumbnail key={result.id} result={result}  />
             ))}
         </FlipMove>
     )
